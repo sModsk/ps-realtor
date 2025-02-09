5e14ca96c93eee480e5a1cab80612b90a37e5186
Soig <smod01sk@gmail.com>
2025-02-09 18:50:25 +0200

    Added smodsk_shellBuilder support

---
+++ fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+use_experimental_fxv2_oal 'yes'
+
+game "gta5"
+
+author "Xirvin & Project Sloth"
+version '2.0.1'
+
+lua54 'yes'
+
+
+ui_page 'html/index.html'
+-- ui_page 'http://localhost:3000/' --for dev
+
+dependency {
+  'smodsk_shellBuilder'
+}
+
+shared_script {
+  '@ox_lib/init.lua',
+  "shared/**",
+}
+
+server_script {
+  "server/**",
+}
+
+client_script {
+  'client/**',
+}
+
+
+
+files {
+  'html/**',
+}
